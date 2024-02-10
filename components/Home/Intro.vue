2379cbfbc4629bf3068d59bffed2c4c01c4e1154
sujeet.singh <sujeet.singh@greyb.com>
2024-02-10 20:06:55 +0530

    :zap: Initial commit

---
+++ components/Home/Intro.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="space-y-6">
+    <img src="/avatar.png" alt="Sujeet Singh" srcset=""
+      class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
+      sizes="48px sm:64px">
+    <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
+      Hello there! 👋
+    </h1>
+    <p class="text-gray-900 dark:text-gray-400">
+      I'm Sujeet, I work as a software, product engineer and designer fromIndia. I specialize in building web applications and design systems using Javascript, Python, Vue &amp; Node. This is my 52 weeks of code challenge, where I will be building and sharing something new every week. I'm also a huge fan of open-source and love to contribute to the community.
+    </p>
+    <p class="text-gray-900 dark:text-gray-400">
+      By day, I'm a Fullstack Developer at
+      <a href="https://greyb.com" target="_blank" class="underline">GreyB</a>, and
+      by night (and weekends), I'm busy tinkering with some random tool or app
+      that I am building.
+    </p>
+  </div>
+</template>
+
+<script setup>
+useSeoMeta({
+  title: "Sujeet Singh",
+  description:
+    "I'm Sujeet, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, Python, Vue & Node.",
+});
+</script>
---
+++ pages/projects.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="min-h-screen">
+    <AppHeader class="mb-12" title="Projects" :description="description" />
+    <div class="space-y-4">
+      <AppProjectCard
+        v-for="(project, id) in projects"
+        :key="id"
+        :project="project"
+      />
+    </div>
+  </main>
+</template>
+
+<script setup>
+const description =
+  "Over the years, I've worked on many small projects, but these are the ones I'm really proud of. Some of them are open-source, which means anyone can check out the code and suggest improvements if they want to. So feel free to take a look and share your ideas if you have any!";
+useSeoMeta({
+  title: "Projects | Sujeet Singh",
+  description,
+});
+
+const { data: projects } = await useAsyncData("projects-all", () =>
+  queryContent("/projects").find()
+);
+</script>
